9b15d668409edb25e95d56a8c3ed2fc684cc590d
d-german <facmet107@gmail.com>
2018-08-04 21:18:49 -0500

    Initial commit

---
+++ SOLID-Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SOLID-Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SOLID-Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f530196b-9b5b-4268-9e80-4dbf822e66b8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SOLID-Tests/SwapExampleTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SOLID;
+namespace SOLID_Tests
+{
+    [TestClass]
+    public class SwapExampleTest
+    {
+        [TestMethod]
+        public void SwapIntTest()
+        {
+            var a = 10;
+            var b = 20;
+
+            SwapExample.Swap(ref a, ref b);
+
+            Assert.IsTrue(a == 20);
+            Assert.IsTrue(b == 10);
+        }
+    }
+}
